8a89a992e2c9fed7e09a86516fa043a6f962a0d3
natalia zhydejkina <Karina.Varlena@instantconnectnow.com>
2020-09-12 23:04:15 +0300

    lesson 1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 2;
+            int y = 1424;
+            //Console.WriteLine($"{x} {y}");
+            //Console.WriteLine(x + " "+ y);
+
+
+            Console.WriteLine($"{3} {1424}");
+            Console.WriteLine(3 + 1424);
+
+            /* int[] arr = new int[] { 1, 2, 3, 4, 5 };
+             float[] arr2;
+             arr2 = new float[10];
+             for (int i = 0; i < 10; i++) {
+                 arr2[i - 1] = i;
+             }
+             int j = 0;
+             while ( j< 10)
+             {
+                 Console.WriteLine(arr2[j]);
+                 j++;
+             }*/
+            x = 2;
+            double[] arr = new double[6];
+            for(int i = 0; i<6; i++)
+            {
+                arr[i] = Math.Pow(2, i + 1);
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+}
