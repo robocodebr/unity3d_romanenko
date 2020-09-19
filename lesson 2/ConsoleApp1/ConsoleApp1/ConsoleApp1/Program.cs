8cdfe400e95fb3cdd15ca0383372f1fffd5a7894
robocodebr <robocodebr@gmail.com>
2020-09-19 17:27:12 +0300

    lesson 2

---
+++ lesson 2/ConsoleApp1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String day = "f";
+            switch (day)
+            {
+                case "s":Console.Write("Sunday"); break;
+                case "m": Console.Write("Mounday"); break;
+                case "t": Console.Write("Tuesday"); break;
+                case "w": Console.Write("Wednesday"); break;
+                case "th": Console.Write("Thursday"); break;
+                case "f": Console.Write("Friday"); break;
+                case "sa": Console.Write("Saturday"); break;
+
+                default: Console.Write("noteday"); break;
+
+
+            }
+
+
+
+
+
+        }
+            /*
+            float var1 = 11;
+            double var2 = 12;
+            short var3 = 13;
+            long L = 12L;
+            byte bt = 12;
+            short sht = 13;
+            if (bt > sht)
+            {
+                Console.WriteLine(bt);
+            }
+            else if(bt>=L)
+            {
+                Console.WriteLine(bt);
+            }
+            else
+            {
+
+            }
+            Console.WriteLine("Hello World!");
+            
+                if (var1 > var2 && var1 > var3)
+                {
+                Console.WriteLine(var2);
+            } else if (var2 > var3 && var2 > var1)
+            {
+                Console.WriteLine(var1);
+            }
+            else {
+                Console.WriteLine(var3);*/
+            }
+            
+        }
+    
