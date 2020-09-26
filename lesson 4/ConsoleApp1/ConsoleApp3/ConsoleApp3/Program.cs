545c239d64297980e667980d80958e9c87743e20
robocodebr <robocodebr@gmail.com>
2020-09-26 15:52:38 +0300

    lesson 4

---
+++ lesson 4/ConsoleApp1/ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> l = new List<string>(){"Укр.м"," алгебра", "геометрия", "заруб.л", "рус.яз" };
+
+            foreach(string s in l)
+            {
+                Console.Write(s);
+            }
+
+            Console.WriteLine(l.Count);
+
+            l.Clear();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+             
